1af5d66975774a0ab9ceaa2f64982e1ecb425fa5
rosae <rosae9236@yahoo.es>
2018-10-21 22:14:39 +0100

    segunda entrega

---
+++ EnjoyArts/app/src/main/java/com/example/rosa/enjoyarts/Tab_paintColor.java
@@ -0,0 +1,24 @@
+package com.example.rosa.enjoyarts;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Tab_paintColor extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_tab_paint_color, container, false);
+
+
+
+    }
+}
